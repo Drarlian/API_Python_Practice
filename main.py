9e3583e18559756a8c64981da27e7e11c0ae4d8a
Drarlian <witoredson@gmail.com>
2024-02-27 11:13:39 -0300

    add: email validation for user/admin creation | feat: updating people collection for users

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ pessoas_exemplo = [{"id": 1, "nome": "Joao", "idade": 35, "role": "Admin"},
 
 
 # Modelo **Pydantic** para validar os dados de uma nova Pessoa:
-class Pessoa(BaseModel):
+class User(BaseModel):
     cpf: str
     name: str
     first_name: str
@@ -78,7 +78,7 @@ class Admin(BaseModel):
     age: int
 
 
-class UpdatePessoa(BaseModel):
+class UpdateUser(BaseModel):
     name: str = None
     first_name: str = None
     last_name: str = None
@@ -98,13 +98,13 @@ class UpdateAdmin(BaseModel):
 
 # Rotas de Pessoas:
 # ROTAS GET:
-@app.get("/pessoas")
-async def get_pessoas():
+@app.get("/user")
+async def get_all_users():
     # pessoas = await db.people.find().to_list(length=None)
     # return pessoas
 
     # Acessar a coleção "people":
-    pessoas = await db.people.find().to_list(length=None)
+    users = await db.users.find().to_list(length=None)
 
     """
     Converte o ObjectId para string.
@@ -112,54 +112,58 @@ async def get_pessoas():
     de forma única. No entanto, o ObjectId não é diretamente serializável para JSON.
     Para corrigir esse erro, devemos converter explicitamente o ObjectId para uma string antes de retornar os dados.
     """
-    for person in pessoas:
-        person["_id"] = str(person["_id"])  # -> Convertendo o _id para uma String.
+    for user in users:
+        user["_id"] = str(user["_id"])  # -> Convertendo o _id para uma String.
 
-    return pessoas
+    return users
 
 
 # Pegando Pessoa Especifica
-@app.get("/pessoas/{cpf}")
-async def get_pessoa_by_cpf(cpf: str):
-    pessoa = await db.people.find_one({"cpf": cpf})
+@app.get("/user/{cpf}")
+async def get_user_by_cpf(cpf: str):
+    user = await db.users.find_one({"cpf": cpf})
 
-    if pessoa:
-        pessoa["_id"] = str(pessoa["_id"])
-        return pessoa
+    if user:
+        user["_id"] = str(user["_id"])
+        return user
     else:
-        return {"message": "Pessoa não encontrada!"}
+        return {"message": "Usuário não encontrado!"}
 
 
 # ROTA POST:
-@app.post("/pessoas/create")
-async def create_pessoa(pessoa: Pessoa):
+@app.post("/user/create")
+async def create_user(user: User):
     # if pessoa.nome == "":
     #     raise HTTPException(status_code=404, detail="O nome não pode estar em branco!")
 
     # nova_pessoa = {"id": len(pessoas) + 1, "nome": pessoa.nome, "idade": pessoa.idade, "role": pessoa.role}
     # pessoas.append(nova_pessoa)
 
-    existe = await procura_cpf(pessoa.cpf, "user")
+    existe_cpf = await procura_cpf(user.cpf, "user")
+    existe_email = await procura_email(user.email, "user")
 
-    if not existe:
-        nova_pessoa = {"cpf": pessoa.cpf, "name": pessoa.name, "first_name": pessoa.first_name,
-                       "last_name": pessoa.last_name, "email": pessoa.email, "age": pessoa.age,
-                       "status": "active", "role": "user"}
-        await db.people.insert_one(nova_pessoa)
+    if (not existe_cpf) and (not existe_email):
+        new_user = {"cpf": user.cpf, "name": user.name, "first_name": user.first_name,
+                    "last_name": user.last_name, "email": user.email, "age": user.age,
+                    "status": "active", "role": "user"}
+        await db.users.insert_one(new_user)
 
         return {"message": "Pessoa criada com sucesso!"}
-    elif existe is None:
+    elif (existe_cpf is None) or (existe_email is None):
         return {"message": "Erro interno."}
     else:
-        return {"message": "Usuário já cadastrado!"}
+        if existe_cpf:
+            return {"message": "CPF já cadastrado!"}
+        elif existe_email:
+            return {"message": "E-mail já cadastrado!"}
 
 
 # ROTA PATCH:
-@app.patch("/pessoas/{cpf}")
-async def update_pessoa(cpf: str, update_infos: UpdatePessoa):
+@app.patch("/user/{cpf}")
+async def update_user_by_cpf(cpf: str, update_infos: UpdateUser):
     update_infos = update_infos.dict(exclude_unset=True)
 
-    request = await db.people.update_one({"cpf": cpf}, {"$set": update_infos})
+    request = await db.users.update_one({"cpf": cpf}, {"$set": update_infos})
 
     # Verifica se exatamente um documento foi modificado durante a operação de atualização: (Usamos o update_one)
     if request.modified_count == 1:
@@ -172,20 +176,20 @@ async def update_pessoa(cpf: str, update_infos: UpdatePessoa):
 
 
 # ROTA DELETE:
-@app.delete("/pessoas/delete/{cpf}")
-async def delete_pessoa(cpf: str):
-    request = await db.people.delete_one({"cpf": cpf})
+@app.delete("/user/delete/{cpf}")
+async def delete_user_by_cpf(cpf: str):
+    request = await db.users.delete_one({"cpf": cpf})
 
     if request.deleted_count == 1:
         return {"message": "Usuário removido com sucesso!"}
     else:
-        return {"message": "Erro ao remover pessoa!"}
+        return {"message": "Erro ao remover usuário!"}
 
 
 # Rotas de Admin:
 # ROTAS GET:
 @app.get("/admin")
-async def get_admin():
+async def get_all_admin():
     admins = await db.admins.find().to_list(length=None)
 
     for admin in admins:
@@ -196,7 +200,7 @@ async def get_admin():
 
 # Pegando Admin Especifico
 @app.get("/admin/{cpf}")
-async def get_pessoa_by_cpf(cpf: str):
+async def get_admin_by_cpf(cpf: str):
     admin = await db.admins.find_one({"cpf": cpf})
 
     if admin:
@@ -209,24 +213,29 @@ async def get_pessoa_by_cpf(cpf: str):
 # ROTA POST:
 @app.post("/admin/create")
 async def create_admin(admin: Admin):
-    existe = await procura_cpf(admin.cpf, "admin")
+    existe_cpf = await procura_cpf(admin.cpf, "admin")
+    existe_email = await procura_email(admin.email, "admin")
 
-    if not existe:
-        novo_admin = {"cpf": admin.cpf, "name": admin.name, "first_name": admin.first_name, "last_name": admin.last_name,
+    if (not existe_cpf) and (not existe_email):
+        novo_admin = {"cpf": admin.cpf, "name": admin.name, "first_name": admin.first_name,
+                      "last_name": admin.last_name,
                       "email": admin.email, "age": admin.age, "status": "active", "role": "admin"}
 
         await db.admins.insert_one(novo_admin)
 
         return {"message": "Admin criado com sucesso!"}
-    elif existe is None:
+    elif (existe_cpf is None) or (existe_email is None):
         return {"message": "Erro interno."}
     else:
-        return {"message": "Usuário já cadastrado!"}
+        if existe_cpf:
+            return {"message": "CPF já cadastrado!"}
+        elif existe_email:
+            return {"message": "E-mail já cadastrado!"}
 
 
 # ROTA PATCH:
 @app.patch("/admin/{cpf}")
-async def update_admin(cpf: str, update_infos: UpdateAdmin):
+async def update_admin_by_cpf(cpf: str, update_infos: UpdateAdmin):
     update_infos = update_infos.dict(exclude_unset=True)
 
     request = await db.admins.update_one({"cpf": cpf}, {"$set": update_infos})
@@ -237,12 +246,12 @@ async def update_admin(cpf: str, update_infos: UpdateAdmin):
         else:
             return {"message": f"Os campos | {' | '.join(list(update_infos.keys()))} | foram alterados com suscesso!"}
     else:
-        return {"message":  "Falha ao atualizar admin!"}
+        return {"message": "Falha ao atualizar admin!"}
 
 
 # ROTA DELETE:
 @app.delete("/admin/delete/{cpf}")
-async def delete_admin(cpf: str):
+async def delete_admin_by_cpf(cpf: str):
     request = await db.admins.delete_one({"cpf": cpf})
 
     if request.deleted_count == 1:
@@ -254,13 +263,27 @@ async def delete_admin(cpf: str):
 # VALIDAÇÕES:
 async def procura_cpf(cpf: str, collection: str) -> bool | None:
     if collection == "user":
-        request = await db.people.find({"cpf": cpf}).to_list(length=None)
+        request = await db.user.find({"cpf": cpf}).to_list(length=None)
     elif collection == "admin":
         request = await db.admins.find({"cpf": cpf}).to_list(length=None)
     else:
         return None
 
-    if request.__len__() != 0:
+    if request.__len__() > 0:
         return True  # -> Retorna True se existe.
     else:
         return False  # -> Retorna False se não existe.
+
+
+async def procura_email(email: str, collection: str) -> bool | None:
+    if collection == "user":
+        request = await db.user.find({"email": email}).to_list(length=None)
+    elif collection == "admin":
+        request = await db.admins.find({"email": email}).to_list(length=None)
+    else:
+        return None
+
+    if request.__len__() > 0:
+        return True
+    else:
+        return False
